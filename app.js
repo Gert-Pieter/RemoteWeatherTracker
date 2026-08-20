e891f1d019b6d156fbc4d4ad720d882b9d8a49fe
G.P. <gpvanzyl3@gmail.com>
2026-08-20 18:04:21 +0200

    Added icons for links, changed style of button as well as added delete button for locations. Fixed onload function not working and added a welcome message if user is new

--- app.js
+++ app.js
@@ -1,46 +1,50 @@
-const map = L.map("map").setView([-25.7479, 28.2293], 10);
+let SaveArea = null;
+let PreviewMarker = null;
+let lt = null;
+let ln = null ;
+let saveLocation = JSON.parse(localStorage.getItem("areas")) || [];
+let weatherInfomartion = document.getElementById("weatherInformation")
+const map = L.map("map",{
+    center: [-25.7479, 28.2293], 
+    zoom: 10,
+    preferCanvas: true,
+    zoomSnap: 1,
+});
 
+// Add OpenStreetMap tile layer at the bottem right of map
 L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution: "&copy; OpenStreetMap"
 }).addTo(map);
 
-
-let SaveArea = null;
-let PreviewCircle = null;
-let lt = null;
-let ln = null ;
-let saveLocation = JSON.parse(localStorage.getItem("saveLocation")) || [];
-
 map.on("click", function (e) {
     lt = e.latlng.lat;
     ln = e.latlng.lng;
 
-    if (PreviewCircle != null) {
-        map.removeLayer(PreviewCircle)
+    // Remove previous marker if it exists
+    if (PreviewMarker != null) {
+        map.removeLayer(PreviewMarker)
     }
-    // Create new circle
-    PreviewCircle = L.marker([lt, ln]).addTo(map);  
+    // Create new marker
+    PreviewMarker = L.marker([lt, ln]).addTo(map);  
 });
 
 function selectArea()
-{
+{   
+    firstTime = false;
+    if (lt === null || ln === null)    
+        {
+            return
+        }
 
-if (lt === null || ln === null) {
-    return
-}
-
-SaveArea = {lt: lt,ln:ln}
-saveLocation.push(SaveArea) 
-localStorage.setItem("areas",JSON.stringify(saveLocation))
-
-let infocards = document.createElement('div')
-let weatherInfomartion = document.getElementById("weatherInformation")
-weatherdata(lt,ln);
-
-ShowAllLocations()
+    SaveArea = {lt: lt,ln:ln}
+    saveLocation.push(SaveArea) 
+    localStorage.setItem("areas",JSON.stringify(saveLocation))
+    weatherdata(lt,ln);
+    window.location.reload();
 };
 
+// Fetch weather data for a given location
 async function weatherdata(lat,lon) {
     try 
     {
@@ -51,7 +55,6 @@ async function weatherdata(lat,lon) {
             
         }
         const data = await responses.json();
-        console.log(data);
         return data;
       } 
     catch (error) {
@@ -60,25 +63,43 @@ async function weatherdata(lat,lon) {
 }
 
 async function ShowAllLocations(){
-    let AllLocation = document.getElementById("weatherInformation")
-    AllLocation.innerHTML = "";
+    if (saveLocation.length === 0)
+        {   
+            console.log("hello");
+            const div = document.createElement("div");
+            div.setAttribute('id','welcomeMessage')
+            div.innerHTML = '<h1>Welcome!</h1><p>To get started select a area on the map and press the button save Location in order to choose that location.<br>Once you have saved a location, the weather information will be displayed here. You can also delete your saved locations at any time.</p>';
+            weatherInfomartion.appendChild(div)
+        }
+        else{
+
+    weatherInfomartion.innerHTML = "";
     for (let locataion of saveLocation)
         {
             let data = await weatherdata(locataion.lt,locataion.ln)
-            console.log(data);
             const div = document.createElement("div");
             div.setAttribute('class','LocCard')
 
             div.innerHTML = `
-            <h2>Weather Area</h2>
+            <h2>Location ${saveLocation.indexOf(locataion) + 1}</h2>
             <p>Temperature: ${data.current.temperature_2m} °C</p>
             <p>Humidity: ${data.current.relative_humidity_2m} %</p>
             <p>Wind: ${data.current.wind_speed_10m} km/h</p>
+
+            <button onclick="deleteLocation(${locataion.lt}, ${locataion.ln})" id="deleteBtn">Delete</button>
             `;
     
-        AllLocation.appendChild(div)
-    }
+        weatherInfomartion.appendChild(div)
+    }}
+}
+window.addEventListener("DOMContentLoaded", async function () {
+    await ShowAllLocations();
+});
 
 
-
+function deleteLocation(lat, lon) {
+    // Filter out the location to be deleted
+    saveLocation = saveLocation.filter(location => !(location.lt === lat && location.ln === lon));
+    localStorage.setItem("areas", JSON.stringify(saveLocation));
+    window.location.reload();
 }
